46f53296684748ecbe6593b1e66caace230f5cb7
tom <tabertz@gmail.com>
2017-04-27 02:24:26 +0000

    defined network for FERClassifier (not tuned)

--- facial-emotion-recognition/src/main/java/com/github/tab269/ml/facialemotionrecognition/FERClassifier.java
+++ facial-emotion-recognition/src/main/java/com/github/tab269/ml/facialemotionrecognition/FERClassifier.java
@@ -6,16 +6,25 @@ import org.datavec.image.loader.NativeImageLoader;
 import org.datavec.image.recordreader.ImageRecordReader;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.*;
+import org.deeplearning4j.nn.conf.distribution.Distribution;
+import org.deeplearning4j.nn.conf.distribution.GaussianDistribution;
+import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.*;
 import org.deeplearning4j.nn.modelimport.keras.InvalidKerasConfigurationException;
-import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.modelimport.keras.UnsupportedKerasConfigurationException;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,8 +49,8 @@ public class FERClassifier {
         final int rngseed = 6;
         final Random randNumGen = new Random(rngseed);
         final int batchSize = 128;
-        final int outputNum = 7;
-        final int numEpochs = 5;
+        final int numLabels = 7;
+        final int numEpochs = 50;
 
 
         final String baseDir = "E:/tiefenrausch/data/fer2013/fer2013/";
@@ -57,8 +66,8 @@ public class FERClassifier {
         ImageRecordReader rrTest = new ImageRecordReader(height, width, channels, labelMaker);
         rrTest.initialize(fileSplitTest);
 
-        DataSetIterator iteratorTrain = new RecordReaderDataSetIterator(rrTrain, batchSize,1, outputNum);
-        DataSetIterator iteratorTest = new RecordReaderDataSetIterator(rrTest, batchSize,1, outputNum);
+        DataSetIterator iteratorTrain = new RecordReaderDataSetIterator(rrTrain, batchSize,1, numLabels);
+        DataSetIterator iteratorTest = new RecordReaderDataSetIterator(rrTest, batchSize,1, numLabels);
 
         // Scale pixel values to 0-1
         DataNormalization scalerTrain = new ImagePreProcessingScaler(0,1);
@@ -70,14 +79,63 @@ public class FERClassifier {
         iteratorTest.setPreProcessor(scalerTest);
 
 
-        log.info("**** Import Model ****");
-        MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights("model-transport/src/main/resources/model.json", "model-transport/src/main/resources/model.h5");
+        log.info("**** Build Model ****");
+
+        double nonZeroBias = 1;
+        double dropOut = 0.5;
+        int iterations = 1;
+        int seed = 42;
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .weightInit(WeightInit.DISTRIBUTION)
+                .dist(new NormalDistribution(0.0, 0.01))
+                .activation(Activation.RELU)
+                .updater(Updater.NESTEROVS)
+                .iterations(iterations)
+                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer) // normalize to prevent vanishing or exploding gradients
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .learningRate(1e-2)
+                .biasLearningRate(1e-2*2)
+                .learningRateDecayPolicy(LearningRatePolicy.Step)
+                .lrPolicyDecayRate(0.1)
+                .lrPolicySteps(100000)
+                .regularization(true)
+                .l2(5 * 1e-4)
+                .momentum(0.9)
+                .miniBatch(false)
+                .list()
+                .layer(0, convInit("cnn1", channels, 96, new int[]{11, 11}, new int[]{4, 4}, new int[]{3, 3}, 0))
+                .layer(1, new LocalResponseNormalization.Builder().name("lrn1").build())
+                .layer(2, maxPool("maxpool1", new int[]{3,3}))
+                .layer(3, conv5x5("cnn2", 256, new int[] {1,1}, new int[] {2,2}, nonZeroBias))
+                .layer(4, new LocalResponseNormalization.Builder().name("lrn2").build())
+                .layer(5, maxPool("maxpool2", new int[]{3,3}))
+                .layer(6,conv3x3("cnn3", 384, 0))
+                .layer(7,conv3x3("cnn4", 384, nonZeroBias))
+                .layer(8,conv3x3("cnn5", 256, nonZeroBias))
+                .layer(9, maxPool("maxpool3", new int[]{2,2}))
+                .layer(10, fullyConnected("ffn1", 4096, nonZeroBias, dropOut, new GaussianDistribution(0, 0.005)))
+                .layer(11, fullyConnected("ffn2", 4096, nonZeroBias, dropOut, new GaussianDistribution(0, 0.005)))
+                .layer(12, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .name("output")
+                        .nOut(numLabels)
+                        .activation(Activation.SOFTMAX)
+                        .build())
+                .backprop(true)
+                .pretrain(false)
+                .setInputType(InputType.convolutional(height, width, channels))
+                .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.init();
         model.setListeners(new ScoreIterationListener(10));
 
+        log.info("******TRAIN MODEL******");
+        model.fit(iteratorTrain);
 
         log.info("******EVALUATE TRAINED MODEL******");
-        Evaluation evaluation = new Evaluation(outputNum);
+        Evaluation evaluation = new Evaluation(numLabels);
         while (iteratorTest.hasNext()) {
             DataSet ds = iteratorTest.next();
             INDArray prediction = model.output(ds.getFeatures(), false);
@@ -85,4 +143,25 @@ public class FERClassifier {
         }
         log.info(evaluation.stats());
     }
+
+    private static ConvolutionLayer convInit(String name, int in, int out, int[] kernel, int[] stride, int[] pad, double bias) {
+        return new ConvolutionLayer.Builder(kernel, stride, pad).name(name).nIn(in).nOut(out).biasInit(bias).build();
+    }
+
+    private static SubsamplingLayer maxPool(String name, int[] kernel) {
+        return new SubsamplingLayer.Builder(kernel, new int[]{2,2}).name(name).build();
+    }
+
+    private static ConvolutionLayer conv5x5(String name, int out, int[] stride, int[] pad, double bias) {
+        return new ConvolutionLayer.Builder(new int[]{5,5}, stride, pad).name(name).nOut(out).biasInit(bias).build();
+    }
+
+    private static ConvolutionLayer conv3x3(String name, int out, double bias) {
+        return new ConvolutionLayer.Builder(new int[]{3,3}, new int[] {1,1}, new int[] {1,1}).name(name).nOut(out).biasInit(bias).build();
+    }
+
+    private static DenseLayer fullyConnected(String name, int out, double bias, double dropOut, Distribution dist) {
+        return new DenseLayer.Builder().name(name).nOut(out).biasInit(bias).dropOut(dropOut).dist(dist).build();
+    }
+
 }
